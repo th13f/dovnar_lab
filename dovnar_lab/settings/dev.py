548956cf48ddbba5a59b143488aab4c458a142f0
th13f <roman@razortheory.com>
2014-11-11 16:47:03 +0300

    added static

--- dovnar_lab/settings/dev.py
+++ dovnar_lab/settings/dev.py
@@ -80,7 +80,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, './static')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, '../../templates'),
--- dovnar_lab/urls.py
+++ dovnar_lab/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -30,4 +31,5 @@ urlpatterns = patterns('base.views',
 urlpatterns += patterns('', url(r'^media/(?P<path>.*)$',
                                 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, })
 )
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
